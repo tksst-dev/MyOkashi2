71ad1afcd7a7c519eed5876fe1755b9ad4a274bc
T.Shutta <tksst51@gmail.com>
2022-08-20 16:31:55 +0900

    Initial Commit

---
+++ MyOkashi2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyOkashi2
+//
+//  Created by 習田武志 on 2022/08/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
